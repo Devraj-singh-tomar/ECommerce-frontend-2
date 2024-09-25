41616a8f566f4aadc1bc62b10d25cb1c7764fbcf
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-25 11:44:56 +0530

    feat: created home page UI with product-card

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
+
 const Home = () => {
-  return <div>Home</div>;
+  const addToCartHandler = () => {};
+
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        NEW PRODUCTS
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="qweqwe"
+          name="Camera"
+          price={234234}
+          stock={12}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/91xnO7qHAeL._AC_UY436_FMwebp_QL65_.jpg"
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
