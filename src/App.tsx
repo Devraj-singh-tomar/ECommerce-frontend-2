00388839c525be3012ac37ad92edb122a04a370b
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-25 17:33:18 +0530

    feat: created login page UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
+const Login = lazy(() => import("./pages/login"));
 
 // ADMIN ROUTES IMPORTS
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -38,6 +39,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* not logged in route */}
+          <Route path="/login" element={<Login />} />
           {/* loggedIN user routes */}
           <Route>
             <Route path="/shipping" element={<Shipping />} />
